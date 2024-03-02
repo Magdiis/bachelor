ff1c73819fd68dfaef91de54760e02e1e3d91769
Magdiis <magdis.klimesova@seznam.cz>
2024-03-02 22:16:54 +0100

    chat page
    list of chats
    chat row where color is dynamicly changes. Text overflow is ellipsis

---
+++ astromate/src/components/chat/chatRow.vue
@@ -0,0 +1,128 @@
+<template>
+   
+        <ion-row class="row ion-align-items-center ion-nowrap">
+        <ion-col size="auto">
+            <span class="dot" >
+                <ion-icon :icon="isOneProfile? personOutline : peopleOutline" 
+                :class="returnColorClass +' '+ returnIconClass"  >
+            </ion-icon></span>
+        </ion-col>
+        
+        <ion-col class="right">
+            <ion-row style="display: flex;">
+                <h3 class="ion-no-margin text" :class="returnColorClass">Heading aefaefcaec e  sefsefsef</h3>
+            </ion-row>
+            <ion-row>
+                <ion-label class="ion-margin-top text">
+                    adadwadwawdd dawdawdawdawd awd awd awd awd awdaw
+                </ion-label>
+            </ion-row>
+        </ion-col>
+        </ion-row>
+
+  
+</template>
+
+<script setup lang="ts">
+import { colorsCases } from '@/model/createGroupEnums';
+import {IonIcon, IonText,IonLabel, IonCol, IonGrid, IonRow, onIonViewDidEnter } from '@ionic/vue';
+import {personOutline, peopleOutline, personRemoveSharp} from 'ionicons/icons'
+import {computed } from 'vue'
+
+ const props = defineProps<{
+    isOneProfile: boolean,
+    //nameOfGroup: string, 
+    //namesOfProfiles: string[],
+    color: colorsCases
+}>() 
+
+/* style="position: absolute; left: 1em; top: 3em;" */
+
+
+ const returnColorClass = computed(() => {
+    switch(props.color) { 
+   case colorsCases.Blue: { 
+      return "custom-blue"
+   } 
+   case colorsCases.Green: { 
+      return "custom-green"
+   }  
+   case colorsCases.Orange: { 
+      return "custom-orange" 
+   }  
+   case colorsCases.Red: { 
+      return "custom-dark-red" 
+   }
+   default: { 
+      //statements; 
+      break; 
+   } 
+} 
+})
+ const returnIconClass = computed(()=>{
+    if(props.isOneProfile){
+        return "isOneProfile"
+    } else {
+        return "isMoreProfiles"
+    }
+ })
+
+
+//const headingColor = computed<string>(() => {
+//    return "text-magdik-blue"
+//})
+
+// const headingColor2 = computed(() => {
+//     return {
+//         color: "var(--ion-color-blue)"
+//     }
+// })
+</script>
+
+<style scoped>
+.dot {
+  height: 4em;
+  width: 4em;
+  background-color: #fff;
+  border-radius: 50%;
+  border-style: solid;
+  border-width: 0.09em;
+  border-color: #B0ABAB;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+ion-icon.isOneProfile{
+  font-size: 1.8em;
+  --ionicon-stroke-width: 32px;
+}
+
+ion-icon.isMoreProfiles{
+    font-size: 2.4em;
+    --ionicon-stroke-width: 26px;
+}
+
+.ion-margin-top{
+    margin-top: 8px  /*default 16px */
+}
+.row {
+  display: flex;
+  align-items: center;
+  width: 100%;
+}
+
+
+.right {
+  flex: 1;
+  min-width: 0px;
+  display: flex;
+  flex-direction: column;
+}
+
+.text {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+}
+</style>
--- astromate/src/views/chat/ChatPage.vue
+++ astromate/src/views/chat/ChatPage.vue
@@ -7,15 +7,42 @@
     </ion-header>
 
     <ion-content :fullscreen="true">
-
+    <ion-list>
+      <ion-list-header>
+        <h3>Moje vytvořené skupiny</h3>
+      </ion-list-header>
+      
+      <ion-item>
+        <chat-row :color="colorsCases.Red" :is-one-profile="false"></chat-row>
+      </ion-item>
+      
+    </ion-list>
+    <ion-list>
+      <ion-list-header>
+        <h3>Ostatní</h3>
+      </ion-list-header>
+      <ion-item>
+        <chat-row :color="colorsCases.Green" :is-one-profile="true"></chat-row>
+      </ion-item>
+      <ion-item>
+        <chat-row :color="colorsCases.Orange" :is-one-profile="false"></chat-row>
+      </ion-item>
+    </ion-list>
         </ion-content>
         </ion-page>
 
 </template>
 <script setup lang="ts">
 import { IonPage, IonContent, IonTitle, 
-IonHeader, IonToolbar, IonButton, IonRow, IonCol, IonGrid} from '@ionic/vue';
+IonHeader, IonToolbar,IonLabel, IonButton, IonRow, IonCol, IonGrid, IonList, IonListHeader, IonItem} from '@ionic/vue';
+import chatRow from '@/components/chat/chatRow.vue'
+import { colorsCases } from '@/model/createGroupEnums';
+import {personOutline, peopleOutline, personRemoveSharp} from 'ionicons/icons'
+
 </script>
 
 <style scoped>
+h3{
+font-weight: bold;
+}
 </style>
