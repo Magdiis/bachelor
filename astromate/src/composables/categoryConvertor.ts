839ab5aecda63df7e226beddbe8893d1851b7622
Magdiis <magdis.klimesova@seznam.cz>
2024-03-04 22:18:09 +0100

    update category convertor
    category will never be empty string

--- astromate/src/composables/categoryConvertor.ts
+++ astromate/src/composables/categoryConvertor.ts
@@ -2,21 +2,45 @@ import {useCase, useCasesValues, workCases, workCasesValues, SportCases,
     sportCasesValues, colorsCases, colorsCasesValues} from '@/model/createGroupEnums'
 import {ref} from 'vue'
 
-export function convertCategory(useCaseparam: string, categoryParam: string) {
+export function convertCategory(useCaseParam: string, categoryParam: string) {
     var sportCaseThis: string = ""
     var workCaseThis: string = ""
 
     if (categoryParam == "") {
         return {sportCaseThis, workCaseThis}
-    } if (useCaseparam == useCase.Work) {
+    } if (useCaseParam == useCase.Work) {
         workCaseThis = categoryParam
         return {sportCaseThis, workCaseThis}
     } else {
-        if (useCaseparam == useCase.Sport){
+        if (useCaseParam == useCase.Sport){
             sportCaseThis = categoryParam
             return {sportCaseThis, workCaseThis}
         } else {
             return {sportCaseThis, workCaseThis}
         }
     }
+}
+
+export function returnCategory(useCaseParam:string, workCaseParam:string, sportCaseParam:string): string {
+    // if(useCaseParam == useCase.Work){
+    //     return workCaseParam
+    // } if (useCaseParam == useCase.Sport) {
+    //     return sportCaseParam
+    // } else {
+    //     return ""
+    // }
+    switch (useCaseParam) {
+        case useCase.Work:
+            return workCaseParam
+        case useCase.Sport:
+            return sportCaseParam
+        case useCase.Friendship:
+            return useCase.Friendship
+        case useCase.Relationship:
+            return useCase.Relationship
+        default:
+            console.log(`Sorry, we are out of ${useCaseParam}.`);
+            return ""
+    }
+
 }
