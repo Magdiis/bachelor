b1ababd0c4b556dabfe234d812a040f8799b592d
Magdiis <magdis.klimesova@seznam.cz>
2024-03-16 22:01:08 +0100

    logout
    - logout using button with icon - just before making design

--- astromate/src/views/profile/ProfilePage.vue
+++ astromate/src/views/profile/ProfilePage.vue
@@ -2,6 +2,9 @@
   <ion-page>
     <ion-header>
       <ion-toolbar>
+        <ion-buttons slot="end">
+          <ion-button @click="logout()"> <ion-icon slot="icon-only" :icon="logOutOutline"></ion-icon> </ion-button>
+        </ion-buttons>
         <ion-title>Profil</ion-title>
       </ion-toolbar>
     </ion-header>
@@ -15,16 +18,31 @@
 </template>
 
 <script setup lang="ts">
-import {IonContent, IonHeader, IonText, IonPage, IonTitle, IonToolbar, onIonViewWillEnter} from "@ionic/vue";
+import {IonContent, IonHeader,IonIcon, IonText, IonPage, IonTitle, IonToolbar,IonButtons, IonButton, onIonViewWillEnter} from "@ionic/vue";
 import {ref} from "vue";
 import {auth} from "@/firebase-service";
 import {globalProfile} from "@/composables/store/profileStore";
+import {logOutOutline} from "ionicons/icons";
+import authentication from "@/composables/authentication/authentication";
+import {useRouter} from "vue-router";
+import {routesNames} from "@/router/routesNames";
 
 
 
 // TODO: get info about user from Pinia
 
 const profile = globalProfile
+const router = useRouter()
+
+function logout(){
+  authentication().logout()
+  navigate()
+}
+
+function navigate(){
+  router.push({name: routesNames.Login})
+}
+
 
 </script>
 
