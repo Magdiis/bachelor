31558be4f65a7e730b3b065db336e523b05f2d1e
Magdiis <magdis.klimesova@seznam.cz>
2024-04-21 14:19:50 +0200

    ephemerisis

---
+++ astromate/src/composables/empheremis/degreeConvertor.ts
@@ -0,0 +1,17 @@
+
+
+export function getDegree(longitude: string): number {
+    const degree = longitude.substring(0, longitude.indexOf("°"))
+    return Number(degree) + getDegreeByRoundedMinutes(longitude)
+}
+
+export function getDegreeByRoundedMinutes(longitude: string): number {
+    var minutes: string = longitude.split('°')[1]
+    minutes = minutes.substring(0, minutes.indexOf("'"))
+    console.log("minutes: ",minutes)
+   if (Number(minutes) < 30){
+       return 0
+   } else {
+       return 1
+   }
+}
