8e3400bb04ef46351ad28b025359a57c3d85a480
Magdiis <magdis.klimesova@seznam.cz>
2024-04-21 20:55:15 +0200

    validation edit profile page

--- astromate/src/views/profile/EditProfilePage.vue
+++ astromate/src/views/profile/EditProfilePage.vue
@@ -11,12 +11,15 @@
 
     <ion-content :fullscreen="true" class="ion-padding-vertical">
           <div class="ion-padding-horizontal">
-            <ion-input :counter="true" maxlength="20" label="Přezdívka" fill="outline"
+            <ion-input class="custom" :counter="true" maxlength="20" label="Přezdívka" fill="outline"
+                       :helper-text="validationValues.isUsernameEmpty ? 'povinné': validationValues.notUniqueUsername ? 'tato přezdívka je již obsazena' : '' "
                        label-placement="floating" error-text="Špatná přezdívka" v-model="editedProfile.name"></ion-input>
           </div>
 
       <div class="ion-padding-horizontal ion-margin-top">
-        <ion-textarea :counter="true" maxlength="200" label="Popis" label-placement="floating" v-model="editedProfile.description" fill="outline" rows="3"></ion-textarea>
+        <ion-textarea class="custom"
+                      :helper-text="validationValues.isDescriptionEmpty ? 'povinné': '' "
+                      :counter="true" maxlength="200" label="Popis" label-placement="floating" v-model="editedProfile.description" fill="outline" rows="3"></ion-textarea>
       </div>
 
       <div class="ion-padding-start">
@@ -25,7 +28,7 @@
 
       <div  class="center ion-padding-horizontal">
         <ion-datetime presentation="date"
-                      :max="now"
+                      :max="max"
                       :min="min"
                       name="Datum narození"
                       v-model="editedProfile.date"
@@ -61,6 +64,10 @@
         </ion-button>
       </div>
 
+      <div class="center">
+        <ion-text v-if="validationValues.showErrorMessage" color="danger">Špatně vyplněný profil</ion-text>
+      </div>
+
       <div class="ion-padding">
           <ion-button expand="block" shape="round" @click="saveChanges()" >Uložit</ion-button>
       </div>
@@ -75,8 +82,8 @@
 
 import {
   IonCol,
-    IonRow,
-    IonImg,
+  IonRow,
+  IonImg,
   IonItemDivider,
   IonLabel,
   IonIcon,
@@ -89,7 +96,7 @@ import {
   IonPage,
   IonTextarea,
   IonTitle,
-  IonToolbar, onIonViewWillEnter, IonButtons, IonBackButton, onIonViewDidLeave
+  IonToolbar, onIonViewWillEnter, IonButtons, IonBackButton, onIonViewDidLeave, IonText
 } from "@ionic/vue";
 import {cameraOutline, checkmarkOutline, locationOutline, save} from 'ionicons/icons';
 import {computed, reactive, ref} from "vue";
@@ -100,6 +107,7 @@ import {savePicture, usePhotoGallery} from "@/composables/photos/usePhotoGallery
 import updateInFirestore from "@/composables/updateInFirestore";
 import {routesNames} from "@/router/routesNames";
 import {useGeolocation} from "@/composables/geolocation/useGeolocation";
+import fetchingFirebase from "@/composables/fetchingFromFirestore";
 
 const { takePhoto, photo } = usePhotoGallery();
 const profileStore = useProfileStore()
@@ -111,6 +119,14 @@ const editedProfile = reactive<Profile>({date: "", description: "", id: "", name
 const loading = ref(false)
 
 const buttonName = ref<string>("aktualizovat polohu")
+const fetchFromFirebase = fetchingFirebase()
+
+const validationValues = reactive({
+  isUsernameEmpty: false,
+  isDescriptionEmpty: false,
+  notUniqueUsername: false,
+  showErrorMessage: false
+})
 
 onIonViewWillEnter(()=>{
   setProfile()
@@ -118,6 +134,10 @@ onIonViewWillEnter(()=>{
 
 onIonViewDidLeave(()=>{
   cleanPhoto()
+  validationValues.showErrorMessage = false
+  validationValues.isUsernameEmpty = false
+  validationValues.isDescriptionEmpty = false
+  validationValues.notUniqueUsername = false
 })
 
 function setProfile(){
@@ -153,25 +173,54 @@ async function getLocation(){
 
 
 async function saveChanges(){
-  loading.value = true
-  profileStore.setProfile(editedProfile)
-  await (updateInFirestore().updateProfile(editedProfile))
-  if(photo.value != undefined){
-    await savePicture(photo.value)
+  const validation = await validate(editedProfile.name)
+  if(validation){
+    loading.value = true
+    profileStore.setProfile(editedProfile)
+    await (updateInFirestore().updateProfile(editedProfile))
+    if(photo.value != undefined){
+      await savePicture(photo.value)
+    }
+    await router.push({name:routesNames.Profile})
+    loading.value = false
+  } else {
+    validationValues.showErrorMessage = true
   }
-  await router.push({name:routesNames.Profile})
-  loading.value = false
+
 }
 
 const now = computed(()=>{
-  const now = new Date(Date.now())
-  return now.toISOString()
+  return new Date(Date.now())
 })
 
 const min = computed(()=>{
-  const now = new Date(Date.now())
-  return new Date(now.getFullYear() - 120,now.getMonth(),now.getDate()).toISOString()
+  return new Date(now.value.getFullYear() - 120,now.value.getMonth(),now.value.getDate()).toISOString()
 })
+
+const max = computed(()=>{
+  return new Date(now.value.getFullYear() - 18,now.value.getMonth(),now.value.getDate()).toISOString()
+})
+
+async function validate(username: string): Promise<boolean> {
+  validationValues.isUsernameEmpty = editedProfile.name.length < 1
+  validationValues.isDescriptionEmpty = editedProfile.description.length < 1
+  if(!validationValues.isDescriptionEmpty && !validationValues.isUsernameEmpty){
+    if(username !== globalProfile.name){
+      const profile = await fetchFromFirebase.isUsernameExist(username)
+      if(profile){
+        validationValues.notUniqueUsername = true
+        return false
+      } else {
+        return true
+      }
+    } else {
+      return true
+    }
+  } else {
+    return false
+  }
+}
+
 
 
 </script>
