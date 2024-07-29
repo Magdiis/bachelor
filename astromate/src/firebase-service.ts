dc9c4d7fb882cff83776f207c92a0b5a9a6b284c
Magdiis <magdis.klimesova@seznam.cz>
2024-07-29 18:58:52 +0200

    return back changes after rebease

--- astromate/src/firebase-service.ts
+++ astromate/src/firebase-service.ts
@@ -2,13 +2,25 @@ import {initializeApp} from "firebase/app"
 import "firebase/firestore";
 import { collection, getFirestore } from "firebase/firestore";
 import {myConfig} from "@/config";
+import {getAuth} from "firebase/auth";
+import {getStorage, ref} from "firebase/storage";
 
-  export const firebaseApp = initializeApp(myConfig.firebase)
-  export const db = getFirestore(firebaseApp)
-  export const profiles_collection = collection(db, 'profiles')
-  export const groups_collection = collection(db,'groups')
-  export const users_collection = collection(db, "users")
+export const firebaseApp = initializeApp(myConfig.firebase)
+export const db = getFirestore(firebaseApp)
+
+export const auth = getAuth(firebaseApp);
 
 
+export const profiles_collection = collection(db, 'profiles')
+export const groups_collection = collection(db, 'groups')
+export const users_collection = collection(db, "users")
+export const decision_collection = collection(db, "decision")
+
+export const notification_collection = collection(db,"notifications")
+export const groups_chat_collection = collection(db,"groupChat")
+
+export const chats_collection = collection(db,"chat")
 
 
+export const storage = getStorage(firebaseApp);
+const storageRef = ref(storage);
